812270e0d751ea3e06b4585f4f7993bc303828ae
AnuragChinni <67939056+AnuragChinni@users.noreply.github.com>
2020-07-07 19:51:22 +0530

    Create object2.js

---
+++ object2.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+
+let myobj ={
+    
+    "1":"one",
+    "11": 1,
+    "name":"arun",
+
+}
+myobj.ten="ten";
+
+console.log(myobj);
+});
