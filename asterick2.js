ec539638400646f2131e4341316fa02befdec5c9
AnuragChinni <67939056+AnuragChinni@users.noreply.github.com>
2020-07-07 20:30:39 +0530

    Create asterick2.js

---
+++ asterick2.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+
+
+console.log("*","*","*","*","*");
+console.log("*","*","*","*","*");
+console.log("*","*","*","*","*");
+console.log("*","*","*","*","*");
+console.log("*","*","*","*","*");
+});
