d18471626c48702fb27fa9331aa816176a033652
AnuragChinni <67939056+AnuragChinni@users.noreply.github.com>
2020-07-07 09:39:21 +0530

    Create oddoreven.js

---
+++ oddoreven.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+let a = userInput[0];
+  if(a%2==0)
+    console.log("Even");
+  else
+    console.log("Odd");
+});
