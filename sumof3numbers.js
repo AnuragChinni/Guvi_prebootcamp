ab78e4967f56b115b07413c9e6808e2023d8d65f
AnuragChinni <67939056+AnuragChinni@users.noreply.github.com>
2020-07-07 11:07:24 +0530

    Create sumof3numbers.js

---
+++ sumof3numbers.js
@@ -0,0 +1,21 @@
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+ 
+let a = userInput[0].split(" ").map(val => +val);
+
+console.log(a[0]+a[1]+a[2]);
+ 
+ 
+});
