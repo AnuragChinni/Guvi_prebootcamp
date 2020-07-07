d0ef7d7c04ec56191395a4bb7130ad6d2aa622c9
AnuragChinni <67939056+AnuragChinni@users.noreply.github.com>
2020-07-07 11:34:41 +0530

    Create isosceles triangle area.js

---
+++ isosceles triangle area.js
@@ -0,0 +1,22 @@
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+ 
+let a = userInput[0].split(" ").map(val => +val);
+let base = a[0];
+let height = a[1];
+console.log(0.5*base*height);
+ 
+ 
+});
